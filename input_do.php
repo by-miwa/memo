39509ce205026d14e46f0866e8d8b2f40ce0703f
by-miwa <miwa.boardgame@gmail.com>
2021-01-12 12:00:27 +0900

    61. フォームからの情報を保存する②

--- input_do.php
+++ input_do.php
@@ -22,7 +22,9 @@
   try {
     $db = new PDO('mysql:dbname=mydb;host=localhost;charset=utf8','root','root');
 
-    $db->exec('INSERT INTO memos SET memo="' . $_POST['memo']. '", created_at=NOW()');
+    $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
+    $statement->execute(array($_POST['memo']));
+    echo 'メッセージが登録されました';
   } catch(PDOException $e) {
       echo 'DB接続エラー: ' . $e->getMessage();
   }
